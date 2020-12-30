dfd136e8da9534651ff3a3e11bf16a4c95d40434
shehab-shepl <46675464+shehab-shepl@users.noreply.github.com>
2020-12-30 13:21:12 +0200

    begin again from the start

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -76,10 +76,8 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR, 'db.cnf'),
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
